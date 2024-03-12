69a22f0ce61a2b464d8c6a76aef7ffcbd3c5d556
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-03-12 17:29:33 +0300

    Fix

--- config/server.js
+++ config/server.js
@@ -1,12 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", "https://anakolchina97-roan.vercel.app/"),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET"),
-    },
-  },
   app: {
     keys: env.array("APP_KEYS"),
   },
