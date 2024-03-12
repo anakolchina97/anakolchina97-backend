ea0b2ae35a4d25d206685eea5d0f4e41cdec7590
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-03-12 16:03:53 +0300

    Add url & admin for ssl

--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "https://anakolchina97-roan.vercel.app/"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
